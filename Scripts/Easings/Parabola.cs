6baa30c6cf24e6f45d48016755141958f9ac73de
Chuan Liu <chuanliu87@gmail.com>
2018-09-18 00:35:47 +0100

    Now Parabola belongs to PingPong. Also added a new Ease -- the Sine Wave.

--- Scripts/Easings/Parabola.cs
+++ Scripts/Easings/Parabola.cs
@@ -4,15 +4,25 @@ namespace DUCK.Tween.Easings
 {
 	public static partial class Ease
 	{
-		public static float Parabola(float x)
+		public static class PingPong
 		{
-			var sqrRoot = 2f * x - 1f;
-			return 1f - sqrRoot * sqrRoot;
-		}
+			private const float PI2 = Mathf.PI * 2f;
 
-		public static float PingPong(float x)
-		{
-			return 1f - Mathf.Abs(2f * x - 1f);
+			public static float Linear(float x)
+			{
+				return 1f - Mathf.Abs(2f * x - 1f);
+			}
+
+			public static float Parabola(float x)
+			{
+				var sqrRoot = 2f * x - 1f;
+				return 1f - sqrRoot * sqrRoot;
+			}
+
+			public static float Sine(float x)
+			{
+				return (Mathf.Sin(x * PI2) + 1.0f) * 0.5f;
+			}
 		}
 	}
 }
