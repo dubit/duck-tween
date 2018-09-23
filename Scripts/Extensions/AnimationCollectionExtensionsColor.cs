ea4bbb7af0f2cb43ee80d690433d35fb058a0fda
Chuan Liu <chuanliu87@gmail.com>
2018-09-23 03:52:03 +0100

    Added the Sprite Renderer fade support.

--- Scripts/Extensions/AnimationCollectionExtensionsColor.cs
+++ Scripts/Extensions/AnimationCollectionExtensionsColor.cs
@@ -24,6 +24,20 @@ namespace DUCK.Tween.Extensions
 		}
 
 		/// <summary>
+		/// Adds a ColorFadeToAnimation to the animation collection
+		/// </summary>
+		/// <param name="animationCollection">The animation collection to add to</param>
+		/// <param name="renderer">The renderer who's color will be faded</param>
+		/// <param name="to">The end color of the fade</param>
+		/// <param name="duration">The duration of the animation in seconds, defaults to 1f</param>
+		/// <param name="easingFunction">The easing function that will be used to interpolate with</param>
+		/// <returns>Returns this AnimationCollection to comply with fluent interface</returns>
+		public static AnimationCollection ColorFadeTo(this AnimationCollection animationCollection, Renderer renderer, Color to, float duration = 1f, Func<float, float> easingFunction = null)
+		{
+			return animationCollection.Add(renderer.ColorFadeTo(to, duration, easingFunction));
+		}
+
+		/// <summary>
 		/// Adds a FadeAnimation to the animation collection
 		/// </summary>
 		/// <param name="animationCollection">The animation collection to add to</param>
@@ -36,6 +50,49 @@ namespace DUCK.Tween.Extensions
 		public static AnimationCollection Fade(this AnimationCollection animationCollection, Renderer renderer, float from, float to, float duration = 1f, Func<float, float> easingFunction = null)
 		{
 			return animationCollection.Add(renderer.Fade(from, to, duration, easingFunction));
+		}
+
+		/// <summary>
+		/// Adds a FadeToAnimation to the animation collection
+		/// </summary>
+		/// <param name="animationCollection">The animation collection to add to</param>
+		/// <param name="renderer">The renderer who's alpha will be faded</param>
+		/// <param name="to">The end alpha value of the fade</param>
+		/// <param name="duration">The duration of the animation in seconds, defaults to 1f</param>
+		/// <param name="easingFunction">The easing function that will be used to interpolate with</param>
+		/// <returns>Returns this AnimationCollection to comply with fluent interface</returns>
+		public static AnimationCollection FadeTo(this AnimationCollection animationCollection, Renderer renderer, float to, float duration = 1f, Func<float, float> easingFunction = null)
+		{
+			return animationCollection.Add(renderer.FadeTo(to, duration, easingFunction));
+		}
+
+		/// <summary>
+		/// Adds a FadeAnimation to the animation collection
+		/// </summary>
+		/// <param name="animationCollection">The animation collection to add to</param>
+		/// <param name="renderer">The renderer who's alpha will be faded</param>
+		/// <param name="from">The start alpha value of the fade</param>
+		/// <param name="to">The end alpha value of the fade</param>
+		/// <param name="duration">The duration of the animation in seconds, defaults to 1f</param>
+		/// <param name="easingFunction">The easing function that will be used to interpolate with</param>
+		/// <returns>Returns this AnimationCollection to comply with fluent interface</returns>
+		public static AnimationCollection Fade(this AnimationCollection animationCollection, SpriteRenderer renderer, float from, float to, float duration = 1f, Func<float, float> easingFunction = null)
+		{
+			return animationCollection.Add(renderer.Fade(from, to, duration, easingFunction));
+		}
+
+		/// <summary>
+		/// Adds a FadeToAnimation to the animation collection
+		/// </summary>
+		/// <param name="animationCollection">The animation collection to add to</param>
+		/// <param name="renderer">The renderer who's alpha will be faded</param>
+		/// <param name="to">The end alpha value of the fade</param>
+		/// <param name="duration">The duration of the animation in seconds, defaults to 1f</param>
+		/// <param name="easingFunction">The easing function that will be used to interpolate with</param>
+		/// <returns>Returns this AnimationCollection to comply with fluent interface</returns>
+		public static AnimationCollection FadeTo(this AnimationCollection animationCollection, SpriteRenderer renderer, float to, float duration = 1f, Func<float, float> easingFunction = null)
+		{
+			return animationCollection.Add(renderer.FadeTo(to, duration, easingFunction));
 		}
 	}
 }
